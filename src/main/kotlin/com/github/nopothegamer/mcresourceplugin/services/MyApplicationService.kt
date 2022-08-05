d2e31afb4ddd5a8cf71c29c82656fc4b57537f4a
GitHub Action <action@github.com>
2022-08-05 08:53:54 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/nopothegamer/mcresourceplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.nopothegamer.mcresourceplugin.services
+
+import com.github.nopothegamer.mcresourceplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
