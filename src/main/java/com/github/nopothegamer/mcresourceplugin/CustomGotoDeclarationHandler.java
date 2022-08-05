a1a7232417b9fb0f32ad356d447f5eba577f499f
nopo <noahogno@gmail.com>
2022-08-06 02:24:24 +1000

    stuff for publishing

--- src/main/java/com/github/nopothegamer/mcresourceplugin/CustomGotoDeclarationHandler.java
+++ src/main/java/com/github/nopothegamer/mcresourceplugin/CustomGotoDeclarationHandler.java
@@ -15,22 +15,39 @@ import java.util.regex.Pattern;
 
 
 public class CustomGotoDeclarationHandler implements GotoDeclarationHandler {
-    private static final Pattern RESOURCE_PATTERN = Pattern.compile("(\\w+)?:?((.*/)?(.*))");
+    private static final Pattern RESOURCE_PATTERN = Pattern.compile("(\\w+):((.*/)?(.*))");
+    private static final Pattern RESOURCE_PATTERN2 = Pattern.compile("new *.*\\((.*), *((.*/)?(.*))\\)");
     private static final Pattern FILE_PATTERN = Pattern.compile("(.*/main/).*");
+    String sourceText = "";
     @Override
     public PsiElement @Nullable [] getGotoDeclarationTargets(@Nullable PsiElement sourceElement, int offset, Editor editor) {
         if (sourceElement == null) return new PsiElement[0];
         if (!sourceElement.toString().equals("PsiJavaToken:STRING_LITERAL")) return new PsiElement[0];
-        String sourceText = sourceElement.getContext().getText().replace("\"", "");
+        assert sourceElement.getContext() != null;
+        sourceText = sourceElement.getContext().getText().replace("\"", "");
         Matcher resourceMatcher = RESOURCE_PATTERN.matcher(sourceText);
-        if (!resourceMatcher.matches()) return new PsiElement[0];
+        Matcher resourceMatcher2 = RESOURCE_PATTERN2.matcher(sourceText);
+        if (resourceMatcher.matches()) {
+
+        } else {
+            sourceText = sourceElement.getContext().getContext().getContext().getText().replace("\"", "");
+            resourceMatcher2 = RESOURCE_PATTERN2.matcher(sourceText);
+            if (resourceMatcher2.matches()) {
+            } else return new PsiElement[0];
+        }
 
 
         List<PsiElement> psiElements = new ArrayList<>();
         String path = sourceElement.getContainingFile().getVirtualFile().getPath();
+        System.out.println(path);
         Matcher pathMatcher = FILE_PATTERN.matcher(path);
         if (pathMatcher.matches()) {
-            path = pathMatcher.group(1) + "resources/assets/" + resourceMatcher.group(1) + "/" + resourceMatcher.group(2);
+            if (resourceMatcher.matches()) {
+                path = pathMatcher.group(1) + "resources/assets/" + resourceMatcher.group(1) + "/" + resourceMatcher.group(2);
+            } else {
+                path = pathMatcher.group(1) + "resources/assets/" + resourceMatcher2.group(1) + "/" + resourceMatcher2.group(2);
+            }
+            System.out.println(path);
             VirtualFile a = VirtualFileManager.getInstance().findFileByUrl("file://" + path);
             if (a != null && a.exists()) {
                 psiElements.add(PsiManager.getInstance(sourceElement.getProject()).findFile(a));
